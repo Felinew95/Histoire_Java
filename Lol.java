dd49cf8b4975fb133d4835fc42339e475b434e8b
Felinew95 <alexandreburin50@gmail.com>
2026-02-02 17:26:39 +0000

    modified:   Lol.java

--- Lol.java
+++ Lol.java
@@ -1,6 +1,7 @@
 public class Lol {
 
     public static void main(String[] args) {
-        System.out.println("hello")
+        System.out.println("hello");
+        System.out.println("world");
     }
 }
